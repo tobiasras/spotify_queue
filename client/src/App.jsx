234495b2052fcf8c6cc86029cd7db53e28f22a92
Magnus <magn902m@stud.kea.dk>
2023-06-21 12:06:48 +0200

    clean up by deleting unused files, and move stuff around

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import FrontpagePage from "./pages/FrontpagePage";
 import AdminPage from "./pages/adminPage/AdminPage";
