b9788f47d3be2e82502970732a475cde6a1893e1
tobiasras <tobiasras.job@gmail.com>
2023-08-13 14:31:50 +0200

    created secret password on admin page

--- client/src/components/AdminPanel/Parts/musicController.js
+++
@@ -1,7 +0,0 @@
-
-export const playerFetch = (type, method) => {
-    const serverUrl = process.env.REACT_APP_SERVER_URL
-    return fetch(`${serverUrl}/player/${type}`, {
-        method: method
-    })
-}
