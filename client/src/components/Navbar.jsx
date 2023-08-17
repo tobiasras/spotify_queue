43ec31a93f6ddb7ab62736b898650c39e676429e
magn902m <magnus3509@hotmail.com>
2023-08-17 09:48:17 +0200

    refactor navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,77 +5,41 @@ export const Navbar = () => {
         {
             herf: "/",
             herfName: "Queue",
-            icon: "material-symbols-outlined",
-            iconName: "Home",
+            iconName: "home",
         },
         {
             herf: "/addSong",
-            herfName: "playlist_add",
-            icon: "material-symbols-outlined",
-            iconName: "add song",
+            herfName: " Add song",
+            iconName: "playlist_add",
         },
         {
             herf: "/info",
-            herfName: "info",
-            icon: "material-symbols-outlined",
-            iconName: "info",
+            herfName: "Info",
+            iconName: "settings",
         },
     ];
 
-    console.log(navItems);
-
     return (
-        <nav className="sticky bottom-0 z-50 w-full bg-neutral-800 col-[1/4]">
-            <div className="grid p-4 max-w-lg grid-cols-3 mx-auto font-medium">
-                {/* {navItems.map((items) => {
-                    
-                    <Link
-                        to={items.herf}
-                        className="inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-900 group"
-                    >
-                        <span className="w-6 h-6 mb-1 text-gray-500 group-hover:text-blue-600 material-symbols-outlined">
-                            {items.icon}
-                        </span>
-                        {items}
-                        <span className="text-sm text-gray-500 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-                            {items.herfName}
-                        </span>
-                    </Link>;
-                })} */}
-                <Link
-                    to="/"
-                    className="inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-900 group"
-                >
-                    <span className="w-6 h-6 mb-1 text-gray-500 group-hover:text-blue-600 material-symbols-outlined">
-                        home
-                    </span>
-                    <span className="text-sm text-gray-500 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-                        Queue
-                    </span>
-                </Link>
-
-                <Link
-                    to="/addSong"
-                    className="inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-900 group"
-                >
-                    <span className="w-6 h-6 mb-1 text-gray-500 group-hover:text-blue-600 material-symbols-outlined">
-                        playlist_add
-                    </span>
-                    <span className="text-sm text-gray-500 group-hover:text-blue-600">
-                        add song
-                    </span>
-                </Link>
-
-                <Link
-                    to="/info"
-                    className="inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-900 "
-                >
-                    <span className="w-6 h-6 mb-1 text-gray-500 group-hover:text-blue-600 material-symbols-outlined">
-                        info
-                    </span>
-                    <span className="text-sm text-gray-500 group-hover:text-blue-600 ">info</span>
-                </Link>
-            </div>
+        <nav className="sticky bottom-0 z-50 col-[1/4] w-full bg-neutral-900 p-4">
+            {/* <ul className="grid grid-cols-3 mx-auto max-w-lg font-medium"> */}
+            <ul className="flex justify-center font-medium">
+                {navItems.map((items, index) => (
+                    <li key={index} className="w-32">
+                        <Link
+                            to={items.herf}
+                            // hover:bg-neutral-950
+                            className="flex flex-col items-center justify-center p-2 group"
+                        >
+                            <span className="w-6 h-6 text-gray-500 group-hover:text-white material-symbols-outlined">
+                                {items.iconName}
+                            </span>
+                            <span className="text-sm text-gray-500 group-hover:text-white dark:group-hover:text-white">
+                                {items.herfName}
+                            </span>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </nav>
     );
 };
